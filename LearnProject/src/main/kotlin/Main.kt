9e28da1d49e396564e25343a34cea245f7480107
Mateusz Walczyk <mattiusw@gmail.com>
2024-04-01 16:27:20 +0200

    Test variable

--- LearnProject/src/main/kotlin/Main.kt
+++ LearnProject/src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 fun main(args: Array<String>) {
 
+    var a = 20
+    var b = 10
+    var c = '!'
+    var s = "Hello World Kotlin"
+
+    println("Wynik " + "A: " + a + " + "+ "B: " + b + " równa się = " + (a + b))
+    println(s + c)
 
 }

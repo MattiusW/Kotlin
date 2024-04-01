8d4cfbd1e9446ad7d498e931bacd912c62a3317e
Mateusz Walczyk <mattiusw@gmail.com>
2024-04-01 16:31:15 +0200

    Full variable record

--- LearnProject/src/main/kotlin/Main.kt
+++ LearnProject/src/main/kotlin/Main.kt
@@ -8,4 +8,11 @@ fun main(args: Array<String>) {
     println("Wynik " + "A: " + a + " + "+ "B: " + b + " równa się = " + (a + b))
     println(s + c)
 
+    //var zmienna: Typ = wartość
+    println("Pełny zapis w Kotlinie:")
+    println("var zmienna: Typ = wartość")
+
+    var number: Float = 10.0F;
+    println(number)
+
 }

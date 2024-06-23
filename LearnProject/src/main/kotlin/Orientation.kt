d3ac9fd5435d7be5196621174fb32953735e9052
Mateusz Walczyk <mattiusw@gmail.com>
2024-06-23 10:59:05 +0200

    Tryb wyliczeniowy pomaga w czytelnosci kodu

---
+++ LearnProject/src/main/kotlin/Orientation.kt
@@ -0,0 +1,15 @@
+enum class Orientation {
+
+    PORTRAIT, LANDSCAPE, REVERSER_PORTRAIT, REVERSED_LANDSCAPE
+
+}
+
+fun setOrientation(orientation: Orientation) {
+    when(orientation) {
+        Orientation.PORTRAIT -> TODO()
+        Orientation.LANDSCAPE -> TODO()
+        Orientation.REVERSER_PORTRAIT -> TODO()
+        Orientation.REVERSED_LANDSCAPE -> TODO()
+    }
+}
+
